2cda7926bb93f3c1096012eba1d9e00db99b733c
squishedfox <christopher.schoeder@protonmail.com>
2024-10-17 20:47:57 -0500

    setup application as base template

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # desktop
 
 Desktop Application Built with using Fyne to use freight cms
+
+## Getting Started
+
+### Debian
+
+Make sure you have the following packages installed to be able to compile the go modules
+
+```sh
+sudo apt-get install golang gcc libgl1-mesa-dev xorg-dev libxinerama-dev
+```
--- main.go
+++ main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Freight CMS")
+	links := []string{"Home", "About", "Contact"}
+	navigationList := widget.NewList(func() int {
+		return len(links)
+	}, func() fyne.CanvasObject {
+		return widget.NewLabel("A template")
+	}, func(id widget.ListItemID, object fyne.CanvasObject) {
+		object.(*widget.Label).SetText(links[id])
+	})
 
-	content := widget.NewLabel("A label")
-	w.SetContent(container.NewVBox(
-		content,
-		widget.NewButton("Hi!", func() {
-			content.SetText("Welcome :)")
-		}),
-	))
+	tabs := container.NewAppTabs(
+		container.NewTabItem("Tab 1", container.NewStack(
+			widget.NewForm(
+				widget.NewFormItem("Name", widget.NewEntry()),
+				widget.NewFormItem("DBA", widget.NewEntry()),
+			), // end NewForm
+		)),
+	)
+	main := container.New(layout.NewGridLayout(2), navigationList, tabs)
 
-	w.ShowAndRun()
+	w.SetContent(main)
+	w.Resize(fyne.NewSize(1024, 768))
+	w.Show()
+
+	a.Run()
 }
