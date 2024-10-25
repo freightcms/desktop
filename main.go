6110a23977c3f5ecc6e3cd2c1be4bf03dff4fc41
squishedfox <christopher.schoeder@protonmail.com>
2024-10-24 21:59:16 -0500

    simplifies adding and removing tabs

--- main.go
+++ main.go
@@ -25,37 +25,8 @@ func NewTab(text string, action func(string)) *container.TabItem {
 
 func closeTab(tabContainer *container.AppTabs, index int) {
 	logging.Logger.Debug("Closing Tab")
-	items := tabContainer.Items
-	selectedIndex := tabContainer.SelectedIndex()
-	if index >= len(items) {
-		return
-	}
-	if index == len(items)-1 {
-		items = items[:index]
-	} else if index == 0 {
-		items = items[1:]
-	} else {
-		items = append(items, items[:index]...)
-		items = append(items, items[index+1:]...)
-	}
-
-	tabContainer.SetItems(items)
-	// we need to figure out where the user was in the list of tabs. It's possible that there are no tabs that can be selected by the user at this point
-	if selectedIndex == 0 && len(items) == 0 {
-		if len(items) == 0 {
-			// user has closed all tabs we should now close the window
-			tabContainer.SelectIndex(0)
-		} else {
-			// there are more items but we can set the selected index still to 0
-			tabContainer.SelectIndex(0)
-		}
-	} else if selectedIndex >= len(items) {
-		// the user was selected on the last index so lets go to the left
-		tabContainer.SelectIndex(selectedIndex - 1)
-	} else {
-		// we can just select whatever tab was to the left the user
-		tabContainer.SelectIndex(selectedIndex + 1)
-	}
+	tabContainer.RemoveIndex(index)
+	logging.Logger.Debug("Selected index", tabContainer.SelectedIndex())
 }
 
 // appendTab handles the scenario for when user is clicking on the "new" tab item to add a new tab to their application
@@ -68,10 +39,8 @@ func appendTab(tabContainer *container.AppTabs) {
 	logging.Logger.Debug("Appending Tab")
 	// TODO; we want to insert the new tab rather than just select it
 	tabToAdd := container.NewTabItemWithIcon("Home", theme.HomeIcon(), widget.NewLabel("Getting Started"))
-	items := tabContainer.Items
-	items = append(items, tabToAdd)
-	tabContainer.SetItems(items)
-	tabContainer.SelectIndex(len(items) - 1) // select the new tab for the user
+	tabContainer.Append(tabToAdd)
+	tabContainer.SelectIndex(len(tabContainer.Items) - 1)
 }
 
 var (
@@ -95,14 +64,16 @@ func main() {
 		}),
 	)
 
-	tabContainer.OnSelected = func(item *container.TabItem) {
-		if item.Text == "New" {
-			appendTab(tabContainer)
-		}
-	}
+	toolbar := widget.NewToolbar(
+		widget.NewToolbarAction(theme.LoginIcon(), func() {
+			logging.Logger.Debug("User Clicked Login. Not Implemented.")
+		}),
+	)
+	main := container.New(layout.NewVBoxLayout(), toolbar, widget.NewSeparator(), tabContainer)
 
-	main := container.New(layout.NewStackLayout(), tabContainer)
-
+	//
+	// setup short cuts
+	// these need to be after the fact because we reference the unboxed variable tabContaine
 	w.Canvas().AddShortcut(newTabShortcut, func(shortcut fyne.Shortcut) {
 		logging.Logger.Debug("New Tab Shortcut")
 		appendTab(tabContainer)
