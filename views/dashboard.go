72c043ec0a9b5dbb0988041fd4292035db8eddf9
squishedfox <christopher.schoeder@protonmail.com>
2024-10-26 19:57:23 -0500

    Update dashboard to load proof of concept

--- views/dashboard.go
+++ views/dashboard.go
@@ -3,19 +3,25 @@ package views
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
+	_ "fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	_ "github.com/freightcms/desktop/logging"
+	"github.com/freightcms/desktop/logging"
 )
 
 func NewHomePage() *fyne.Container {
-	homePageLayout := layout.NewGridLayoutWithColumns(4)
-	organizationCard := widget.NewCard("Organizations", "Manage", widget.NewIcon(theme.SettingsIcon()))
-
-	return container.New(
-		homePageLayout,
+	return container.NewPadded(
 		container.NewCenter(
-			container.New(homePageLayout, organizationCard)),
+			container.NewGridWithColumns(4,
+				container.NewPadded(
+					widget.NewButtonWithIcon("Organizations", theme.HomeIcon(), func() {
+						logging.Logger.Debug("Click on settings")
+					}),
+					widget.NewButtonWithIcon("Settings", theme.SettingsIcon(), func() {
+						logging.Logger.Debug("Click on settings")
+					}),
+				),
+			),
+		),
 	)
 }
