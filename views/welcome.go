18288b97a031302a29eccb3a409c37326eed49de
squishedfox <christopher.schoeder@protonmail.com>
2024-11-09 10:20:51 -0600

    Create custom welcome text

--- views/welcome.go
+++ views/welcome.go
@@ -2,14 +2,27 @@ package views
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/widget"
 )
+
+func welcomeLabel() fyne.CanvasObject {
+	style := fyne.TextStyle{
+		Bold: true,
+	}
+	txt := &canvas.Text{
+		Alignment: fyne.TextAlignCenter,
+		Text:      "Welcome!",
+		TextSize:  36,
+		TextStyle: style,
+	}
+	return txt
+}
 
 func WelcomeView() *fyne.Container {
 	return container.New(
 		layout.NewCenterLayout(),
-		widget.NewLabel("Welcome!"),
+		welcomeLabel(),
 	)
 }
