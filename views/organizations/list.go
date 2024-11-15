bd5c63235d381f28ccf50a05199791218f59fd89
squishedfox <christopher.schoeder@protonmail.com>
2024-11-14 20:54:43 -0600

    wip

--- views/organizations/list.go
+++ views/organizations/list.go
@@ -2,6 +2,7 @@ package organizations
 
 import (
 	"desktop/logging"
+	"fmt"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -10,13 +11,57 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-func NewTable() fyne.CanvasObject {
+type OrganizationItemViewModel struct {
+}
+
+type OrganizationTableViewModel struct {
+	page     int
+	pageSize int
+	sortBy   string
+	items    []*OrganizationItemViewModel
+}
+
+func NewOrganizationTableViewModel() *OrganizationTableViewModel {
+	return &OrganizationTableViewModel{
+		page:     0,
+		pageSize: 10,
+		sortBy:   "id",
+		items:    []*OrganizationItemViewModel{},
+	}
+}
+
+// Length gets the number of records available
+func (v *OrganizationTableViewModel) Length() int {
+	return len(v.items)
+}
+
+// At gets the organization view item at the index specified. If the index is out of bounds
+// of the array then the function returns `nil`, and `false`. If the index somehow fetches
+// a `nil` value from the items then it will return `nil` and `false` as the item was
+// present in the array but stores as `nil`
+func (v *OrganizationTableViewModel) At(i int) (*OrganizationItemViewModel, bool) {
+	if i < 0 || i > len(v.items) {
+		return nil, false
+	}
+	item := v.items[i]
+	return item, item != nil
+}
+
+func NewTable(vm *OrganizationTableViewModel) fyne.CanvasObject {
+	headers := []string{"ID", "Name", "DBA", "Rollup ID"}
 	tbl := widget.NewTableWithHeaders(func() (rows, cols int) {
-		return 1, 4
+		return vm.Length(), 4
 	}, func() fyne.CanvasObject {
 		return widget.NewLabel("Hello World")
 	}, func(i widget.TableCellID, o fyne.CanvasObject) {
-		o.(*widget.Label).SetText("Hello form the other side")
+		if i.Row == 0 {
+			o.(*widget.Label).SetText(headers[i.Col])
+			return
+		}
+		data, ok := vm.At(i.Row)
+		if ok {
+			o.(*widget.Label).SetText(fmt.Sprintf("Row %d, Col %d"))
+		}
 	})
 
 	return tbl
